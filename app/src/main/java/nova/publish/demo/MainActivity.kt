637e0bef52fc8b05b8c18d04abb57ebb31fd6af0
Tung Phan <tungphanthanh247@gmail.com>
2026-02-07 10:14:01 +0700

    refactor(app): Remove direct dependency on mylibrary
    This commit removes the usage of the `SampleDemo` class from the `mylibrary` module within the `MainActivity.kt`.
    
    The `Greeting` composable function is updated to use the `name` parameter directly, instead of calling `SampleDemo.callMe()`. The unused import for `nova.publish.mylibrary.SampleDemo` has also been removed.

--- app/src/main/java/nova/publish/demo/MainActivity.kt
+++ app/src/main/java/nova/publish/demo/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import nova.publish.demo.ui.theme.PublishDemoLibraryTheme
-import nova.publish.mylibrary.SampleDemo
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,9 +32,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val myName = SampleDemo.callMe()
     Text(
-        text = "Hello ${myName}!",
+        text = "Hello ${name}!",
         modifier = modifier
     )
 }
